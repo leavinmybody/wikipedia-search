8bd44a1ae3d4f52f9838fc55e041b57eb5e2371b
Jawad <leavinmybody@gmail.com>
2022-10-08 06:10:53 -0500

    :tada: First commit

---
+++ src/App.js
@@ -0,0 +1,59 @@
+import { useState } from "react";
+
+function App() {
+  const [search, setSearch] = useState("");
+  const [results, setResults] = useState([]);
+  const [searchInfo, setSearchInfo] = useState({});
+
+  const handleSearch = async (e) => {
+    e.preventDefault();
+    if (search === "") return;
+
+    const response = await fetch(
+      `https://en.wikipedia.org/w/api.php?action=query&list=search&origin=*&format=json&srsearch=${search}`
+    );
+
+    if (!response.ok) {
+      throw Error(response.statusText); // throw error if response is not ok
+    }
+
+    const json = await response.json();
+    setResults(json.query.search);
+    setSearchInfo(json.query.searchinfo);
+  };
+
+  return (
+    <div className="App">
+      <header>
+        <h1>React Wikipedia </h1>
+        <form className="search-box" onSubmit={handleSearch}>
+          <input
+            type="search"
+            placeholder="Search..."
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+          />
+        </form>
+        {searchInfo.totalhits ? (
+          <p>Search Results: {searchInfo.totalhits}</p>
+        ) : (
+          ""
+        )}
+      </header>
+      <div className="results">
+        {results.map((result, index) => {
+          const url = `https://en.wikipedia.org/?curid=${result.pageid}`;
+          return (
+            <div className="result" key={index}>
+              <h3>{result.title}</h3>
+              <p dangerouslySetInnerHTML={{ __html: result.snippet }}></p>
+              <a href={url}>Read more</a>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
+
+export default App;
